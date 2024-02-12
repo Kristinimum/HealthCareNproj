5e3e30c47c217272610db9bf961f88ab831cc037
Sam Duncan <samuel.p.duncan@gmail.com>
2024-02-12 14:44:26 -0500

    setup terraform remote backend with S3 and DynamDB; added test bucket creation to be destroyed later.

---
+++ Terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.36.0"
+    }
+  }
+  backend "s3" {
+    encrypt        = true
+    bucket         = "pod2-hcn-022024"
+    key            = "backend/tfstate"
+    dynamodb_table = "tfstate-lock-dynamo"
+    region         = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region                   = "us-east-1"
+}
+
+resource "aws_s3_bucket" "testbucket" {
+  bucket = "testbucketduncan3r38r"
+}
