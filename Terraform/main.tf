bf0789f1a8f4da4297b8fdd20a322d28b450d8ef
Sam Duncan <samuel.p.duncan@gmail.com>
2024-02-16 08:18:17 -0500

    updated code to modules

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,14 +5,13 @@ terraform {
       version = "5.36.0"
     }
   }
-
   backend "s3" {
 
-    encrypt        = true
-    bucket         = "pod2-hcn-022024"
-    key            = "backend/tfstate"
+    encrypt = true
+    bucket  = "pod2-hcn-022024"
+    key     = "backend/tfstate"
     dynamodb_table = "tfstate-lock-dynamo"
-    region         = "us-east-1"
+    region = "us-east-1"
 
   }
 }
@@ -22,3 +21,21 @@ provider "aws" {
 }
 
 
+##############      MODULES     ################
+
+module "buckets_s3" {
+  source = "./modules/s3"
+}
+
+module "cloudfront_dist" {
+  source        = "./modules/cloudfront"
+  bucketID      = module.buckets_s3.bucketID
+  bucket_domain = module.buckets_s3.bucket_domain
+}
+
+
+#############      OUTPUTS      ################
+
+output "website_domain_name" {
+  value = "http://${module.cloudfront_dist.cf_domain_name}"
+}
