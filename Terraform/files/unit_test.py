6e88da0374c21e459189de67dcb1d2e083e5ce19
Angel Chaidez <angelomarchaidez@gmail.com>
2024-02-16 18:10:41 -0800

    python traverse to find the index.html file and compare its title

--- Terraform/files/unit_test.py
+++ Terraform/files/unit_test.py
@@ -1,19 +1,27 @@
-#This if statement will allow us to check if our website name matches in our index.html file.
+#This if statement will allow us to check if our website name matches in our index.html file_name.
 #Conditional statements are case sensitive.
 
-file_path = "index.html"
+#Check for file_name starting in the root of directory for file_name named index.html
+# start file_name path default at root directory and search all subdirectories
+import os
+file_name = "index.html"
 expected_title = "HealthCare Hospital"
-error_message = "Website title did not match expected value"
 
-try:
-    with open(file_path, 'r') as file:
-        html_content = file.read()
-       
-        if expected_title in html_content:
-            print("Website title matches expected value:", expected_title)
-        else:
-            print(error_message)
-except FileNotFoundError:
-    print("File not found:", file_path)
-except Exception as e:
-    print("An error occurred:", str(e))
+# traverse directory and subdirectories to find file_name
+for root, dirs, file_names in os.walk("."):
+    for name in file_names: 
+        if name == file_name:
+            print(os.path.join(root, name))
+            # now check that the title of the html file_name matches the expected value
+            with open(os.path.join(root, name), 'r') as f:
+                html_content = f.read()
+                if expected_title in html_content:
+                    print("Website title matches expected value:", expected_title)
+                else:
+                    print("Website title did not match expected value:", expected_title)
+
+
+
+
+
+
