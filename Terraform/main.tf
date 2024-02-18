e208dde833176d71ff5b6ad3c89338a4dd64d9b1
xabiere-designs <xabieredesigns@gmail.com>
2024-02-18 13:54:48 -0500

    codepipeline_draft

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,21 +7,42 @@ terraform {
   }
   backend "s3" {
 
-    encrypt = true
-    bucket  = "pod2-hcn-022024"
-    key     = "backend/tfstate"
+    encrypt        = true
+    bucket         = "pod2-hcn-022024"
+    key            = "backend/tfstate"
     dynamodb_table = "tfstate-lock-dynamo"
-    region = "us-east-1"
+    region         = "us-east-1"
 
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+
+  assume_role {
+   role_arn = "arn:aws:iam::654654434704:role/CodePipelineServiceRole"
+  }
 }
 
 
+locals {
+  tags = {
+    Project     = var.project
+    CreatedBy   = var.createdBy
+    CreatedOn   = timestamp()
+    Environment = terraform.workspace
+  }
+}
+
 ##############      MODULES     ################
+
+module "codepipeline" {
+  source = "./modules/codepipeline"
+  #source             = "https://github.com/Kristinimum/HealthCareNproj/tree/main/Terraform/modules/codepipeline?ref=main"
+  project_name                   = var.project_name
+  s3_bucket_id                   = var.s3_bucket_id
+  full_repository_id             = var.full_repository_id
+ }
 
 module "buckets_s3" {
   source = "./modules/s3"
